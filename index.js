7c17b64305b980319d93f140f8b72ee110eb1248
yym-yumeng123 <18026493873@163.com>
2023-12-14 13:09:13 +0800

    express

--- index.js
+++ index.js
@@ -1,50 +1,6 @@
-const eventloop = {
-  queue: [],
-  loop() {
-    while (this.queue.length) {
-      var callback = this.queue.shift()
-      callback()
-    }
-    setTimeout(this.loop.bind(this), 50)
-  },
-  add(callback) {
-    this.queue.push(callback)
-  },
-}
+const express = require('express')
+const app = express()
+const port = 3000
 
-eventloop.loop()
-
-setTimeout(() => {
-  eventloop.add(function () {
-    console.log("1", 1)
-  })
-}, 500)
-setTimeout(() => {
-  eventloop.add(function () {
-    console.log("2", 2)
-  })
-}, 500)
-
-!(function () {
-  const promise = new Promise(function (reslove, reject) {
-    setTimeout(() => {
-      reslove(3)
-    }, 500)
-  }).then(function (res) {
-    console.log("res", res)
-  })
-
-  console.log("Promise", promise)
-
-  setTimeout(() => {
-    console.log("promise1", promise)
-  }, 800)
-})()
-
-
-
-
-console.log(async function() {
-  const res = await "yym"
-  console.log('res12',res)
-})
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
