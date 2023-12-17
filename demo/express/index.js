6126151357f501d7c24846f577e2e77bf8fcca99
yym-yumeng123 <18026493873@163.com>
2023-12-17 21:41:36 +0800

    express req res

---
+++ demo/express/index.js
@@ -0,0 +1,10 @@
+/**
+ * express 基于 Node.js web 框架应用
+ * 封装了一些工具包 便于开发HTTP服务
+ */
+
+
+/**
+ * 路由: 应用程序如何响应客户端对特定端点的请求
+ * app.method(path, callback)
+ */
---
+++ demo/express/体验express/index.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.get("/", (req, res) => res.send("Hello World!"))
+
+app.get("/request", (req, res) => {
+  // 获取请求参数
+  console.log("req.method", req.method)
+  console.log("req.path", req.path)
+  console.log("req.query", req.query)
+  res.status(200).json({
+    code: 200,
+  })
+})
+
+app.get("/routers/:id", (req, res) => {
+  // 获取路由参数
+  console.log("req.params.id", req.params.id)
+})
+
+app.post("/response", (req, res) => {
+  // 可以链式调用
+  res.status(200)
+  res.set('xxx', "yyy")
+  res.send("express")
+
+  res.redirect("/xxx") // 跳转
+  res.download() // 下载
+  res.json()
+})
+
+app.get("/home", (req, res) => {
+  res.status(200).json({
+    code: 200,
+  })
+})
+
+app.all("*", (req, res) => {
+  res.end("404")
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
